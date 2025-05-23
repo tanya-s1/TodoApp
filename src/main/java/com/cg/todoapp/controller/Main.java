b3086df2168736c5f05fa741750e0c1341625ef8
ujjwal0903 <ujjwalsinghal0903@gmail.com>
2025-05-23 15:46:52 +0530

    JSON File Handling Added using DAO

--- src/main/java/com/cg/todoapp/controller/Main.java
+++ src/main/java/com/cg/todoapp/controller/Main.java
@@ -4,18 +4,20 @@ import com.cg.todoapp.dao.TodoDao;
 import com.cg.todoapp.dao.TodoDaoImpl;
 import com.cg.todoapp.dao.UserDao;
 import com.cg.todoapp.dao.UserDaoImpl;
-import com.cg.todoapp.repository.TodoRepository;
-import com.cg.todoapp.repository.UserRepository;
 import com.cg.todoapp.service.TodoService;
 import com.cg.todoapp.service.UserService;
 
 public class Main {
     public static void main(String[] args) {
-        UserRepository userRepo = new UserRepository();
-        TodoRepository todoRepo = new TodoRepository();
+        
+        // UserRepository userRepo = new UserRepository();
+        // TodoRepository todoRepo = new TodoRepository();
 
-        UserDao userDao = new UserDaoImpl(userRepo);
-        TodoDao todoDao = new TodoDaoImpl(todoRepo);
+        // UserDao userDao = new UserDaoImpl(userRepo);
+        // TodoDao todoDao = new TodoDaoImpl(todoRepo);
+        
+        UserDao userDao = new UserDaoImpl();
+        TodoDao todoDao = new TodoDaoImpl();
 
         UserService userService = new UserService(userDao);
         TodoService todoService = new TodoService(todoDao);
