3f0a21e89a421572c81f0ae8cc20c6f61a8bfe71
ujjwal0903 <ujjwalsinghal0903@gmail.com>
2025-05-24 11:45:28 +0530

    JavaDoc Created and Repository Folder Commented

--- src/main/java/com/cg/todoapp/repository/TodoRepository.java
+++ src/main/java/com/cg/todoapp/repository/TodoRepository.java
@@ -1,45 +1,45 @@
-package com.cg.todoapp.repository;
+// package com.cg.todoapp.repository;
 
-import java.util.List;
-import java.util.Optional;
+// import java.util.List;
+// import java.util.Optional;
 
-import com.cg.todoapp.entity.Todo;
-import com.cg.todoapp.entity.User;
+// import com.cg.todoapp.entity.Todo;
+// import com.cg.todoapp.entity.User;
 
-public class TodoRepository {
+// public class TodoRepository {
 
-    public void addTodo(User user, Todo todo) {
-        user.getTodos().add(todo);
-    }
+//     public void addTodo(User user, Todo todo) {
+//         user.getTodos().add(todo);
+//     }
 
-    public List<Todo> getTodos(User user) {
-        return user.getTodos();
-    }
+//     public List<Todo> getTodos(User user) {
+//         return user.getTodos();
+//     }
 
-    public Optional<Todo> findById(User user, long id) {
-        return user.getTodos().stream()
-                   .filter(todo -> todo.getId() == id)
-                   .findFirst();
-    }
+//     public Optional<Todo> findById(User user, long id) {
+//         return user.getTodos().stream()
+//                    .filter(todo -> todo.getId() == id)
+//                    .findFirst();
+//     }
 
-    public void deleteById(User user, long id) {
-        user.getTodos().removeIf(todo -> todo.getId() == id);
-    }
+//     public void deleteById(User user, long id) {
+//         user.getTodos().removeIf(todo -> todo.getId() == id);
+//     }
 
-    public void updateTodo(User user, Todo updatedTodo) {
-        List<Todo> todos = user.getTodos();
-        for (int i = 0; i < todos.size(); i++) {
-            if (todos.get(i).getId() == updatedTodo.getId()) {
-                todos.set(i, updatedTodo);
-                break;
-            }
-        }
-    }
+//     public void updateTodo(User user, Todo updatedTodo) {
+//         List<Todo> todos = user.getTodos();
+//         for (int i = 0; i < todos.size(); i++) {
+//             if (todos.get(i).getId() == updatedTodo.getId()) {
+//                 todos.set(i, updatedTodo);
+//                 break;
+//             }
+//         }
+//     }
 
-    public long getNextTodoId(User user) {
-        return user.getTodos().stream()
-                   .mapToLong(Todo::getId)
-                   .max()
-                   .orElse(0) + 1;
-    }
-}
+//     public long getNextTodoId(User user) {
+//         return user.getTodos().stream()
+//                    .mapToLong(Todo::getId)
+//                    .max()
+//                    .orElse(0) + 1;
+//     }
+// }
--- src/main/java/com/cg/todoapp/repository/UserRepository.java
+++ src/main/java/com/cg/todoapp/repository/UserRepository.java
@@ -1,31 +1,31 @@
-package com.cg.todoapp.repository;
+// package com.cg.todoapp.repository;
 
-import com.cg.todoapp.entity.User;
+// import com.cg.todoapp.entity.User;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+// import java.util.HashMap;
+// import java.util.Map;
+// import java.util.Optional;
 
-public class UserRepository {
-    private Map<String, User> users = new HashMap<>();
+// public class UserRepository {
+//     private Map<String, User> users = new HashMap<>();
 
-    public boolean existsByUsername(String username) {
-        return users.containsKey(username);
-    }
+//     public boolean existsByUsername(String username) {
+//         return users.containsKey(username);
+//     }
 
-    public void save(User user) {
-        users.put(user.getUsername(), user);
-    }
+//     public void save(User user) {
+//         users.put(user.getUsername(), user);
+//     }
 
-    public Optional<User> findByUsername(String username) {
-        return Optional.ofNullable(users.get(username));
-    }
+//     public Optional<User> findByUsername(String username) {
+//         return Optional.ofNullable(users.get(username));
+//     }
 
-    public boolean authenticate(String username, String password) {
-        return users.containsKey(username) && users.get(username).getPassword().equals(password);
-    }
+//     public boolean authenticate(String username, String password) {
+//         return users.containsKey(username) && users.get(username).getPassword().equals(password);
+//     }
 
-    public Map<String, User> getAllUsers() {
-        return users;
-    }
-}
+//     public Map<String, User> getAllUsers() {
+//         return users;
+//     }
+// }
